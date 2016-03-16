80a925752c0b32013f07b656056c9de1a50805f4
drewvollrath <drew@vollrath.ca>
2016-03-16 08:27:01 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
+#"#{number} SECOND(S)!" in each iteration of the loop. The method should return "HAPPY NEW YEAR!"
+def countdown(int)
 
-def countdown
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(int)
+  sleep(int)
+end
+
+countdown(10)
